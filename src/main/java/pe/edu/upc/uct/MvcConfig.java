f657dd5f7ca062b55531277815a40539a6487ba3
unknown <marlonwis.luis@gmail.com>
2022-06-30 20:26:00 -0500

    Ey Tamo de vuelta

--- src/main/java/pe/edu/upc/uct/MvcConfig.java
+++ src/main/java/pe/edu/upc/uct/MvcConfig.java
@@ -1,10 +1,8 @@
 package pe.edu.upc.uct;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
